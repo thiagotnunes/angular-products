ee2f8b72df5d062e3f9a90fd02090d9d26fe833a
Thiago Nunes <thiagotnunes@gmail.com>
2012-05-08 11:59:11 -0300

    Playing around with images and angularjs

---
+++ js/accounting_configuration.js
@@ -0,0 +1,14 @@
+accounting.settings = {
+  currency: {
+    symbol : "R$",
+    format: "%s %v",
+    decimal : ".",
+    thousand: ",",
+    precision : 2
+  },
+  number: {
+    precision : 0,  // default precision on numbers is 0
+    thousand: ",",
+    decimal : "."
+  }
+}
--- js/product.js
+++ js/product.js
@@ -1,15 +1,48 @@
 function ProductController($scope) {
   var products = [
     {
-      thumbnail: "http://placehold.it/160x120",
-      profile: "http://placehold.it/60x60",
-      title: "This is the title and it should be aligned left",
-      description: "This is the description and it should be below the title, with at least x characters",
-      ratings: "x ratings",
-      reviews: "x reviews",
-      price: "30.00"
+      thumbnail: "img/products/ferrari.jpg",
+      profile: "img/profiles/me.png",
+      title: "Ferrari",
+      description: "This is a freaking ferrari dude you should totally buy it",
+      ratings: "100 ratings",
+      reviews: "1 reviews",
+      price: "3000000.00"
+    },
+    {
+      thumbnail: "img/products/carrera.jpg",
+      profile: "img/profiles/me.png",
+      title: "Carrera",
+      description: "This is just awesome dude, I am just so happy I have one and you don't",
+      ratings: "300 ratings",
+      reviews: "300 reviews",
+      price: "400000.00"
+    },
+    {
+      thumbnail: "img/products/aventador.jpg",
+      profile: "img/profiles/me.png",
+      title: "El aventador",
+      description: "Yo tengo un aventador and it freaking rules. Wanna have it? Buy it!",
+      ratings: "6000 ratings",
+      reviews: "10 reviews",
+      price: "600000.00"
     }
   ];
 
-  $scope.products = products;
+  var truncate = function(string, length) {
+    if(string.length > length) {
+      return string.substring(0, length) + "...";
+    } else {
+      return string;
+    }
+  };
+
+  $scope.products = function() {
+    _.each(products, function(product) {
+      product.title = truncate(product.title, 45);
+      product.price = accounting.formatMoney(product.price);
+      product.description = truncate(product.description, 73);
+    });
+    return products;
+  }();
 }
