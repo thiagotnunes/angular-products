ed7e60540e97254dfa52c1d1f1d77c31ee27512a
Thiago Nunes <thiagotnunes@gmail.com>
2012-05-08 19:21:27 -0300

    Adding products definition to the server side on nodejs (still hardcoded)

--- application.js
+++ application.js
@@ -4,8 +4,42 @@ var app = express.createServer();
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
 
+var products = [
+  {
+    thumbnail: "img/products/ferrari.jpg",
+      profile: "img/profiles/me.png",
+      title: "Ferrari",
+      description: "This is a freaking ferrari dude you should totally buy it",
+      ratings: "100 ratings",
+      reviews: "1 reviews",
+      price: "3000000.00"
+  },
+  {
+    thumbnail: "img/products/carrera.jpg",
+    profile: "img/profiles/me.png",
+    title: "Carrera",
+    description: "This is just awesome dude, I am just so happy I have one and you don't",
+    ratings: "300 ratings",
+    reviews: "300 reviews",
+    price: "400000.00"
+  },
+  {
+    thumbnail: "img/products/aventador.jpg",
+    profile: "img/profiles/me.png",
+    title: "El aventador",
+    description: "Yo tengo un aventador and it freaking rules. Wanna have it? Buy it!",
+    ratings: "6000 ratings",
+    reviews: "10 reviews",
+    price: "600000.00"
+  }
+];
+
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/products.json', function(req, res) {
+  res.json({ products: products });
 });
 
 app.listen(3000);
--- public/js/product.js
+++ public/js/product.js
@@ -1,34 +1,4 @@
-function ProductController($scope) {
-  var products = [
-    {
-      thumbnail: "img/products/ferrari.jpg",
-      profile: "img/profiles/me.png",
-      title: "Ferrari",
-      description: "This is a freaking ferrari dude you should totally buy it",
-      ratings: "100 ratings",
-      reviews: "1 reviews",
-      price: "3000000.00"
-    },
-    {
-      thumbnail: "img/products/carrera.jpg",
-      profile: "img/profiles/me.png",
-      title: "Carrera",
-      description: "This is just awesome dude, I am just so happy I have one and you don't",
-      ratings: "300 ratings",
-      reviews: "300 reviews",
-      price: "400000.00"
-    },
-    {
-      thumbnail: "img/products/aventador.jpg",
-      profile: "img/profiles/me.png",
-      title: "El aventador",
-      description: "Yo tengo un aventador and it freaking rules. Wanna have it? Buy it!",
-      ratings: "6000 ratings",
-      reviews: "10 reviews",
-      price: "600000.00"
-    }
-  ];
-
+function ProductController($scope, $http) {
   var truncate = function(string, length) {
     if(string.length > length) {
       return string.substring(0, length) + "...";
@@ -37,12 +7,14 @@ function ProductController($scope) {
     }
   };
 
-  $scope.products = function() {
-    _.each(products, function(product) {
-      product.title = truncate(product.title, 45);
-      product.price = accounting.formatMoney(product.price);
-      product.description = truncate(product.description, 73);
+  $scope.fetchProducts = function() {
+    $http.get('/products.json').success(function(data) {
+      $scope.products = data.products;
+      _.each($scope.products, function(product) {
+        product.title = truncate(product.title, 45);
+        product.price = accounting.formatMoney(product.price);
+        product.description = truncate(product.description, 73);
+      });
     });
-    return products;
   }();
 }
